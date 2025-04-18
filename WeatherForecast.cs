e659e360bc38d26d5dbf12a4369564853095183d
TranVanThanh <tranvanthanh31124@gmail.com>
2025-04-18 09:45:24 +0700

    first commit

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace TranVanThanh_2122110005
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
